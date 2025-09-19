b578b3976db1c49f0e69944cfd0a0649af5bd046
Nicolas Dupouy <nicolas.dupouy17@orange.fr>
2025-09-19 12:08:25 +0200

    "Fix mineur au niveau du slider"

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -12,7 +12,7 @@ const Slider = () => {
   );
   const nextCard = () => {
     setTimeout(
-      () => setIndex(index + 1 < byDateDesc.length ? index + 1 : 0),
+      () => setIndex(index + 1 < (byDateDesc?.length ?? 0) ? index + 1 : 0),
       5000
     );
   };
